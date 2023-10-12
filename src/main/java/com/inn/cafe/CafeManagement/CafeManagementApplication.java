cf65d620e641339ae6938687c0881162713fd21e
mohanchakka <mohanchakka2001@gmail.com>
2023-10-12 10:20:13 +0530

    Uploding

---
+++ src/main/java/com/inn/cafe/CafeManagement/CafeManagementApplication.java
@@ -0,0 +1,14 @@
+package com.inn.cafe.CafeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementApplication.class, args);
+		System.out.println("running");
+	}
+
+}
